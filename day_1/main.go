f016eebd868b448e5939ab4ecdb3f30cde0bcf99
Jelle De Boeck <jelle.deboeck@axxes.com>
2021-12-03 20:25:42 +0100

    Finished day 1

--- day_1/main.go
+++ day_1/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"time"
 )
@@ -22,241 +21,48 @@ func loadInput() ([]string, error) {
 	return input, nil
 }
 
-// process fields, so find the valid passports
-func processFields1(input []string) int {
-	field_checker := make(map[string]bool)
-	req_fields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "ecl", "pid", "cid"}
-	// init field checker
-	for _, req_field := range req_fields {
-		field_checker[req_field] = false
+func depth_measurement(input []string) int {
+	depth_cnt := 0
+	previous_measurement := input[0]
+	previous_measurement_int, err := strconv.Atoi(previous_measurement)
+	if err != nil {
+		log.Fatalf("Program failed: %s", err)
 	}
-	var valid_passports = 0
-	var valid_passport = true
-	for _, line := range input {
-		// Reached a new passport so req_fields need to be resetted
-		if line == "" {
-			//fmt.Println("New Passport!")
-			//fmt.Println(field_checker)
-			for _, req_field := range req_fields {
-				if !field_checker[req_field] && req_field != "cid" {
-					// one of the fields on passport is not filled in
-					//fmt.Println("Oei Oei")
-					valid_passport = false
-				}
-			}
-			// reset req fields
-			for _, req_field := range req_fields {
-				field_checker[req_field] = false
-				//fmt.Println(field_checker)
-			}
-			// Check if passport is valid
-			if valid_passport {
-				valid_passports += 1
-			}
-			valid_passport = true
-		} else {
-			// Now we need to process the entire field (use a regex and substring)
-			re := regexp.MustCompile("[: ]")
-			line_split := re.Split(line, -1)
-			//fmt.Println(line_split)
-			for i, field := range line_split {
-				//fmt.Println(field)
-				// Only check even indexes
-				if i%2 == 0 {
-					//fmt.Println(field)
-					field_checker[field] = true
-				}
-			}
+	for _, current_measurement := range input {
+		current_measurement_int, err := strconv.Atoi(current_measurement)
+		if err != nil {
+			log.Fatalf("Program failed: %s", err)
 		}
-
-	}
-	// Check final passport
-	//fmt.Println("New Passport!")
-	//fmt.Println(field_checker)
-	for _, req_field := range req_fields {
-		if !field_checker[req_field] && req_field != "cid" {
-			// one of the fields on passport is not filled in
-			//fmt.Println("Oei Oei")
-			valid_passport = false
+		if current_measurement_int > previous_measurement_int {
+			depth_cnt += 1
 		}
+		previous_measurement_int = current_measurement_int
 	}
-	// reset req fields
-	for _, req_field := range req_fields {
-		field_checker[req_field] = false
-		//fmt.Println(field_checker)
-	}
-	// Check if passport is valid
-	if valid_passport {
-		valid_passports += 1
-	}
-	valid_passport = true
-	return valid_passports
+	return depth_cnt
 }
 
-// process fields, so find the valid passports
-func processFields2(input []string) int {
-	field_checker := make(map[string]bool)
-	req_fields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "ecl", "pid", "cid"}
-	// init field checker
-	for _, req_field := range req_fields {
-		field_checker[req_field] = false
-	}
-	var valid_passports = 0
-	var valid_passport = true
-	for _, line := range input {
-		// Reached a new passport so req_fields need to be resetted
-		if line == "" {
-			//fmt.Println("New Passport!")
-			//fmt.Println(field_checker)
-			for _, req_field := range req_fields {
-				if !field_checker[req_field] && req_field != "cid" {
-					// one of the fields on passport is not filled in
-					//fmt.Println("Oei Oei")
-					valid_passport = false
-				}
+func depth_measurement_2(input []string) int {
+	depth_cnt := 0
+	previous_sum := 0
+	current_sum := 0
+	fmt.Println(len(input))
+	for i, _ := range input {
+		if i < len(input)-2 {
+			current_measurement_int_1, err_1 := strconv.Atoi(input[i])
+			current_measurement_int_2, err_2 := strconv.Atoi(input[i+1])
+			current_measurement_int_3, err_3 := strconv.Atoi(input[i+2])
+			if err_1 != nil || err_2 != nil || err_3 != nil {
+				log.Fatalf("Program failed!!")
 			}
-			// reset req fields
-			for _, req_field := range req_fields {
-				field_checker[req_field] = false
-				//fmt.Println(field_checker)
+			current_sum = current_measurement_int_1 + current_measurement_int_2 + current_measurement_int_3
+			if current_sum > previous_sum && i != 0 {
+				depth_cnt += 1
 			}
-			// Check if passport is valid
-			if valid_passport {
-				valid_passports += 1
-			}
-			valid_passport = true
-		} else {
-			// Now we need to process the entire field (use a regex and substring)
-			re := regexp.MustCompile("[: ]")
-			line_split := re.Split(line, -1)
-			//fmt.Println(line_split)
-			for i, field := range line_split {
-				//fmt.Println(field)
-				// Only check even indexes
-				if i%2 == 0 {
-					//fmt.Println(field)
-					// Need to add a switch case with checks for every passport field
-					//field_checker[field] = true
-					value := line_split[i+1]
-					switch {
-					case field == "byr":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^[0-9]{4}$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						if valid_field {
-							// Parse value to an integer
-							value_int, err := strconv.Atoi(value)
-							if err != nil {
-								log.Fatalf("Program failed: %s", err)
-							}
-							if 1920 <= value_int && value_int <= 2002 {
-								//fmt.Println("Correct!")
-								field_checker[field] = true
-							}
-						}
-					case field == "iyr":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^[0-9]{4}$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						if valid_field {
-							// Parse value to an integer
-							value_int, err := strconv.Atoi(value)
-							if err != nil {
-								log.Fatalf("Program failed: %s", err)
-							}
-							if 2010 <= value_int && value_int <= 2020 {
-								//fmt.Println("Correct!")
-								field_checker[field] = true
-							}
-						}
-					case field == "eyr":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^[0-9]{4}$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						if valid_field {
-							// Parse value to an integer
-							value_int, err := strconv.Atoi(value)
-							if err != nil {
-								log.Fatalf("Program failed: %s", err)
-							}
-							if 2020 <= value_int && value_int <= 2030 {
-								//fmt.Println("Correct!")
-								field_checker[field] = true
-							}
-						}
-					case field == "hgt":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^([0-9]*)cm|in$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						if valid_field {
-							// Parse value to an integer
-							re := regexp.MustCompile("[0-9]*")
-							extracted_value := re.FindString(value)
-							re = regexp.MustCompile("cm|in")
-							hgt_type := re.FindString(value)
-							value_int, err := strconv.Atoi(extracted_value)
-							if err != nil {
-								log.Fatalf("Program failed: %s", err)
-							}
-							if hgt_type == "cm" && 150 <= value_int && value_int <= 193 {
-								//fmt.Println("Correct!")
-								//fmt.Println(extracted_value)
-								field_checker[field] = true
-							}
-							if hgt_type == "in" && 59 <= value_int && value_int <= 76 {
-								//fmt.Println("Correct!")
-								//fmt.Println(extracted_value)
-								field_checker[field] = true
-							}
-						}
-					case field == "hcl":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^#([0-9]|[a-f]){6}$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						field_checker[field] = valid_field
-					case field == "ecl":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^amb|blu|brn|gry|grn|hzl|oth$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						field_checker[field] = valid_field
-					case field == "pid":
-						// First match exactly on 4 digits
-						re := regexp.MustCompile("^[0-9]{9}$")
-						valid_field := re.MatchString(value)
-						//fmt.Println(valid_field)
-						field_checker[field] = valid_field
-					}
-				}
-			}
+			previous_sum = current_sum
 		}
+	}
 
-	}
-	// Check final passport
-	//fmt.Println("New Passport!")
-	//fmt.Println(field_checker)
-	for _, req_field := range req_fields {
-		if !field_checker[req_field] && req_field != "cid" {
-			// one of the fields on passport is not filled in
-			valid_passport = false
-		}
-	}
-	// reset req fields
-	for _, req_field := range req_fields {
-		field_checker[req_field] = false
-		//fmt.Println(field_checker)
-	}
-	// Check if passport is valid
-	if valid_passport {
-		valid_passports += 1
-	}
-	valid_passport = true
-	return valid_passports
+	return depth_cnt
 }
 
 func main() {
@@ -265,13 +71,13 @@ func main() {
 		log.Fatalf("Program failed: %s", err)
 	}
 	start1 := time.Now()
-	valid_passports_1 := processFields1(input)
+	depth_cnt := depth_measurement(input)
 	fmt.Println("Solution 1:")
-	fmt.Printf("Number of valid passports: %d\n", valid_passports_1)
+	fmt.Printf("Number of depth measurements: %d\n", depth_cnt)
 	fmt.Printf("Time: %s\n", time.Since(start1))
 	start2 := time.Now()
-	valid_passports_2 := processFields2(input)
+	depth_cnt_2 := depth_measurement_2(input)
 	fmt.Println("Solution 2:")
-	fmt.Printf("Number of valid passports: %d\n", valid_passports_2)
+	fmt.Printf("Number of depth measurements: %d\n", depth_cnt_2)
 	fmt.Printf("Time: %s\n", time.Since(start2))
 }
