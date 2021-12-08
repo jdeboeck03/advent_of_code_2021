cd5b139e995fd5b1b6b3c53e966a306db13521e4
Jelle De Boeck <jelle.deboeck@axxes.com>
2021-12-08 22:14:55 +0100

    Day 5 finished!

--- day_5/main.go
+++ day_5/main.go
@@ -45,166 +45,142 @@ func remove_white_spaces(input []string) []string {
 	return input
 }
 
-func process_bingo_boards(input []string) [][][]string {
-	// number of bingo boards
-	// fmt.Println(len(input))
-	input = remove_white_spaces(input)
-	board_width := 5
-	board_height := 5
-	number_of_boards := len(input) / board_height
-	var bingo_boards = make([][][]string, number_of_boards)
-	// init board
-	for i := range bingo_boards {
+func create_empty_matrix(height int, width int) [][]int {
+
+	var vents = make([][]int, height)
+	for i := range vents {
 		// height of bingo_board
-		bingo_boards[i] = make([][]string, board_height)
-		for j := range bingo_boards[i] {
-			// width of bingo_board
-			bingo_boards[i][j] = make([]string, board_width)
-		}
+		vents[i] = make([]int, width)
 	}
-	// fill up board
-	board_index := 0
-	board_height_index := 0
-	for i := 0; i < len(input); i++ {
-		input_split := strings.Split(input[i], " ")
-		input_split = remove_white_spaces(input_split)
-		for board_width_index, value := range input_split {
-			bingo_boards[board_index][board_height_index][board_width_index] = value
-		}
-		board_height_index++
-		if board_height_index >= board_height {
-			board_height_index = 0
-			board_index++
-		}
-	}
-	return bingo_boards
+
+	//fmt.Println(len(vents))
+	//fmt.Println(len(vents[0]))
+	return vents
 }
 
-func drawing(bingo_boards [][][]string, bingo_draws string) int {
-	solution := 0
-	winning_board := 0
-	winning_number := 0
-	// fmt.Println(bingo_boards)
-	// fmt.Println(bingo_draws)
-	bingo_draws_split := strings.Split(bingo_draws, ",")
-	// fmt.Println(bingo_draws_split)
-	// Drawing
-	fmt.Println(bingo_boards)
-	quit := false
-	number_of_boards_left := len(bingo_boards)
-	fmt.Println(number_of_boards_left)
-	for _, bingo_draw := range bingo_draws_split {
-		// Check each board for a possible match on bingo board
-		// fmt.Println(bingo_draw)
-		for i, board := range bingo_boards {
-			for j, board_line := range board {
-				for k, board_piece := range board_line {
-					//fmt.Println(board_piece)
-					if board_piece == bingo_draw {
-						// fmt.Println("Het is van dat, we hebben een hit")
-						// fmt.Println(board_piece)
-						// fmt.Println(bingo_draw)
-						bingo_boards[i][j][k] = "x"
+func process_lines(input []string) [][]int {
+	var processed_lines = make([][]int, len(input))
+	for j := range processed_lines {
+		// width of bingo_board
+		processed_lines[j] = make([]int, 4)
+	}
+	for i, line := range input {
+		line_split := strings.Split(line, ",")
+		// Put first and last coordinate in processed lines
+		processed_lines[i][0], _ = strconv.Atoi(line_split[0])
+		processed_lines[i][3], _ = strconv.Atoi(line_split[2])
+		//split the last two coordinates even further
+		line_split_2 := strings.Split(line_split[1], " -> ")
+		processed_lines[i][1], _ = strconv.Atoi(line_split_2[0])
+		processed_lines[i][2], _ = strconv.Atoi(line_split_2[1])
+	}
+	return processed_lines
+}
 
-						// Check if bingo board is complete
-						// Loop through the corresponding row
-
-						row_checking := true
-						bingo := false
-						for l, check_piece := range board_line {
-							//fmt.Println(check_piece)
-							if check_piece == "x" {
-								if l == len(board_line)-1 {
-									// BINGO FIND BINGO BOARD
-									// fmt.Println("BINGO")
-									row_checking = false
-									bingo = true
-									number_of_boards_left -= 1
-									fmt.Println(number_of_boards_left)
-									if number_of_boards_left == 0 {
-										winning_board = i
-										winning_number, _ = strconv.Atoi(bingo_draw)
-										quit = true
-									}
-								}
-							} else {
-								row_checking = false
-							}
-							if !row_checking {
-								break
-							}
-						}
-
-						if quit {
-							break
-						}
-
-						// Loop through the corresponding column
-						// Double loop to check every column
-						// Loop through the corresponding column
-						// Double loop to check every column
-						column_checking := true
-						for m, check_board_line := range board {
-							//fmt.Println(check_board_line[k])
-							if check_board_line[k] == "x" {
-								if m == len(check_board_line)-1 {
-									// BINGO FIND BINGO BOARD
-									// fmt.Println("BINGO")
-									//fmt.Println(bingo_boards)
-									column_checking = false
-									if !bingo {
-										number_of_boards_left -= 1
-										fmt.Println(number_of_boards_left)
-									}
-									if number_of_boards_left == 0 {
-										winning_board = i
-										winning_number, _ = strconv.Atoi(bingo_draw)
-										quit = true
-									}
-								}
-							} else {
-								column_checking = false
-							}
-							if !column_checking {
-								break
-							}
-						}
-					}
-					if quit {
-						break
-					}
-				}
-				if quit {
-					break
-				}
-			}
-			if quit {
-				break
-			}
-		}
-		if quit {
-			break
+func find_vert_horiz_lines(processed_lines [][]int) [][]int {
+	vert_horiz_lines := [][]int{}
+	for _, line := range processed_lines {
+		if (line[0] == line[2]) || (line[1] == line[3]) {
+			vert_horiz_lines = append(vert_horiz_lines, line)
 		}
 	}
-	fmt.Println(bingo_boards)
-	fmt.Println(winning_number)
-	fmt.Println(winning_board)
-	sum := 0
-	// Count score!
-	for _, winning_board_line := range bingo_boards[winning_board] {
-		for _, winning_board_piece := range winning_board_line {
-			if winning_board_piece != "x" {
-				winning_board_piece_int, err := strconv.Atoi(winning_board_piece)
-				if err != nil {
-					log.Fatalf("Program failed: %s", err)
+
+	return vert_horiz_lines
+}
+
+func find_diagonal_lines(processed_lines [][]int) [][]int {
+	diagonal_lines := [][]int{}
+	for _, line := range processed_lines {
+		if (line[0] == line[2]) || (line[1] == line[3]) {
+		} else {
+			diagonal_lines = append(diagonal_lines, line)
+		}
+	}
+	return diagonal_lines
+}
+
+func fill_vents(vents [][]int, vert_horiz_lines [][]int) [][]int {
+
+	for _, line := range vert_horiz_lines {
+		//Check for vertical vents
+		if line[0] == line[2] {
+			width := line[0]
+			if line[1] < line[3] {
+				for i := line[1]; i <= line[3]; i++ {
+					vents[i][width]++
 				}
-				sum += winning_board_piece_int
+			} else if line[3] < line[1] {
+				for i := line[3]; i <= line[1]; i++ {
+					vents[i][width]++
+				}
+			}
+		} else if line[1] == line[3] {
+			height := line[1]
+			if line[0] < line[2] {
+				for i := line[0]; i <= line[2]; i++ {
+					vents[height][i]++
+				}
+			} else if line[2] < line[0] {
+				for i := line[2]; i <= line[0]; i++ {
+					vents[height][i]++
+				}
 			}
 		}
 	}
-	fmt.Println(sum)
-	solution = sum * winning_number
-	return solution
+	return vents
+}
+
+func fill_diagonal_vents(vents [][]int, vert_horiz_lines [][]int) [][]int {
+	for _, line := range vert_horiz_lines {
+		if line[0] > line[2] && line[1] < line[3] {
+			//Start from first coordinate
+			height := line[1]
+			for i := line[0]; i >= line[2]; i-- {
+				vents[height][i]++
+				height++
+			}
+		} else if line[0] > line[2] && line[1] > line[3] {
+			//Start from first coordinate
+			height := line[1]
+			for i := line[0]; i >= line[2]; i-- {
+				vents[height][i]++
+				height--
+			}
+		} else if line[0] < line[2] && line[1] < line[3] {
+			//Start from first coordinate
+			height := line[1]
+			for i := line[0]; i <= line[2]; i++ {
+				vents[height][i]++
+				height++
+			}
+		} else if line[0] < line[2] && line[1] > line[3] {
+			//Start from first coordinate
+			height := line[1]
+			//fmt.Println(line)
+			//fmt.Println(width)
+			//fmt.Println(height)
+			for i := line[0]; i <= line[2]; i++ {
+				//fmt.Println(i)
+				//fmt.Println(height)
+				vents[height][i]++
+				height--
+			}
+			//fmt.Println("Processed")
+			//fmt.Println(vents)
+		}
+	}
+	return vents
+}
+func find_points(filled_vents [][]int) int {
+	number_of_points := 0
+	for _, line := range filled_vents {
+		for _, point := range line {
+			if point >= 2 {
+				number_of_points++
+			}
+		}
+	}
+	return number_of_points
 }
 
 func main() {
@@ -213,16 +189,24 @@ func main() {
 		log.Fatalf("Program failed: %s", err)
 	}
 	start1 := time.Now()
-	bingo_draws := input[0]
-	input = remove_index(input, 0)
 	fmt.Println("Solution 1:")
-	// fmt.Println(bingo_draws)
-	bingo_boards := process_bingo_boards(input)
-	// fmt.Println(bingo_boards)
-	solution := drawing(bingo_boards, bingo_draws)
-	fmt.Printf("Solution: %d\n", solution)
+	processed_lines := process_lines(input)
+	vert_horiz_lines := find_vert_horiz_lines(processed_lines)
+	fmt.Println(vert_horiz_lines)
+	height := 991
+	width := 991
+	vents := create_empty_matrix(height, width)
+	filled_vents := fill_vents(vents, vert_horiz_lines)
+	fmt.Println(filled_vents)
+	number_of_points := find_points(filled_vents)
+	fmt.Printf("number of points: %d\n", number_of_points)
 	fmt.Printf("Time: %s\n", time.Since(start1))
 	start2 := time.Now()
 	fmt.Println("Solution 2:")
+	diagonal_lines := find_diagonal_lines(processed_lines)
+	filled_diagonal_vents := fill_diagonal_vents(filled_vents, diagonal_lines)
+	number_of_diagonal_points := find_points(filled_diagonal_vents)
+	fmt.Println(filled_diagonal_vents)
+	fmt.Printf("number of points: %d\n", number_of_diagonal_points)
 	fmt.Printf("Time: %s\n", time.Since(start2))
 }
