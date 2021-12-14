ebdaf87a390f9b03fb22271f764e4bed970854de
Jelle De Boeck <jelle.deboeck@axxes.com>
2021-12-14 22:02:08 +0100

    FInished processing input for day 8 part 2

--- day_8/main.go
+++ day_8/main.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
+
+type number struct {
+	number int
+	output string
+}
 
 // loads input of file into a slice of integer variables
 func loadInput() ([]string, error) {
@@ -57,6 +63,37 @@ func process_input(input []string) ([]string, []string) {
 	return signal_patterns, output_values
 }
 
+func process_input_2(input []string) ([][][]string, [][][]string) {
+	signal_patterns := make([][][]string, len(input))
+	for i := range signal_patterns {
+		// height of bingo_board
+		signal_patterns[i] = make([][]string, 10)
+	}
+	output_values := make([][][]string, len(input))
+	for i := range output_values {
+		// height of bingo_board
+		output_values[i] = make([][]string, 4)
+	}
+
+	//split input
+	for i, input_line := range input {
+		input_line_split := strings.Split(input_line, " | ")
+		signal_values := strings.Split(input_line_split[0], " ")
+		output_value := strings.Split(input_line_split[1], " ")
+		for j, signal_value := range signal_values {
+			signal_value_split := strings.Split(signal_value, "")
+			sort.Strings(signal_value_split)
+			signal_patterns[i][j] = signal_value_split
+		}
+		for j, output := range output_value {
+			output_split := strings.Split(output, "")
+			sort.Strings(output_split)
+			output_values[i][j] = output_split
+		}
+	}
+	return signal_patterns, output_values
+}
+
 func calculate_total_output_values(output_values []string) int {
 	total_output_count := 0
 	for _, output_line := range output_values {
@@ -87,5 +124,8 @@ func main() {
 	fmt.Printf("Time: %s\n", time.Since(start1))
 	start2 := time.Now()
 	fmt.Println("Solution 2:")
+	signal_patterns_2, output_values_2 := process_input_2(input)
+	fmt.Println(signal_patterns_2)
+	fmt.Println(output_values_2)
 	fmt.Printf("Time: %s\n", time.Since(start2))
 }
